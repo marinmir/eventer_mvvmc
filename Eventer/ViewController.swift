1d803efd73503d078a85a3162b32d34def832c68
Miroshnichenko Marina <marinamirosh99@gmail.com>
2021-01-07 23:03:56 +0300

    Create project.

---
+++ Eventer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventer
+//
+//  Created by Мирошниченко Марина on 07.01.2021.
+//  Copyright © 2021 Мирошниченко Марина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
